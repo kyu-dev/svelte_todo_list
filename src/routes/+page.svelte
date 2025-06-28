a0a665cd424a9ff3ad3cddd467a4d215d3bd84f0
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-06-28 23:31:16 +0200

    Suppression du composant Input.svelte et intégration de la logique de gestion des tâches directement dans la page principale, permettant l'ajout et la gestion des tâches dans l'application To-Do.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
-<script>
-  import Input from "../components/Input.svelte";
+<script lang="ts">
+  let taskList = $state([{ text: "Tester svelte", status: true }]);
+  let newItem = $state("");
+
+  function addToList() {
+    if (newItem.trim() !== "") {
+      taskList = [...taskList, { text: newItem, status: false }];
+      newItem = "";
+    }
+  }
+  function swapStatus(index: number) {
+    taskList = taskList.map((task, i) =>
+      i === index ? { ...task, status: !task.status } : task
+    );
+  }
 </script>
 
 <div class="flex flex-col justify-center w-full">
@@ -9,5 +22,40 @@
       >Make with <img src="/logo.png" alt="logo" class="h-10" /></span
     >
   </h1>
-  <div class="w-full flex justify-center"><Input /></div>
+  <div class="w-full flex flex-col items-center">
+    <form
+      action=""
+      class="border-2 border-orange-600 w-fit rounded-sm overflow-hidden"
+      onsubmit={addToList}
+    >
+      <input
+        type="text"
+        class="focus:outline-none focus:ring-0 px-3 w-80"
+        placeholder="Entrez vos tâches"
+        bind:value={newItem}
+      />
+      <button type="submit" class="bg-orange-600 text-white py-2 px-3"
+        >Add Task</button
+      >
+    </form>
+    <div class="mt-4">
+      {#each taskList as task, index}
+        <div class="flex items-center gap-2">
+          <input
+            type="checkbox"
+            name="status"
+            checked={task.status}
+            onchange={() => swapStatus(index)}
+            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+          />
+          <p>{task.text}</p>
+          {#if task.status === false}
+            <p class="text-red-500">à faire</p>
+          {:else}
+            <p class="text-green-500">fait</p>
+          {/if}
+        </div>
+      {/each}
+    </div>
+  </div>
 </div>
