36f3653e41734c934535b101dee8567510719b6c
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-06-28 18:59:26 +0200

    Ajout d'un nouveau composant Input.svelte et mise à jour de la page principale pour améliorer l'interface utilisateur de l'application To-Do avec un en-tête stylisé.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1>todo</h1>
+<div class="flex justify-center w-full">
+  <h1 class="text-5xl font-semibold text-center py-20">
+    To-Do APP <br />
+    <span class="inline-flex gap-2 text-3xl font-normal"
+      >Make with <img src="/logo.png" alt="logo" class="h-10" /></span
+    >
+  </h1>
+</div>
