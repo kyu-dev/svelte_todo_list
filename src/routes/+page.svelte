78c2d0e6674066df581ba794162d1bbe67ce6055
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-06-28 23:41:54 +0200

    Amélioration de la gestion des tâches dans l'application To-Do : ajout de la prévention de l'envoi du formulaire, mise à jour du style des éléments de la liste et ajout de classes CSS pour une meilleure présentation.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,14 @@
   let taskList = $state([{ text: "Tester svelte", status: true }]);
   let newItem = $state("");
 
-  function addToList() {
+  function addToList(event: Event) {
+    event.preventDefault();
     if (newItem.trim() !== "") {
       taskList = [...taskList, { text: newItem, status: false }];
       newItem = "";
     }
   }
+
   function swapStatus(index: number) {
     taskList = taskList.map((task, i) =>
       i === index ? { ...task, status: !task.status } : task
@@ -15,7 +17,7 @@
   }
 </script>
 
-<div class="flex flex-col justify-center w-full">
+<div class="flex flex-col justify-center w-full min-h-screen bg-gray-100">
   <h1 class="text-5xl font-semibold text-center py-20">
     To-Do APP <br />
     <span class="inline-flex gap-2 text-3xl font-normal"
@@ -25,7 +27,7 @@
   <div class="w-full flex flex-col items-center">
     <form
       action=""
-      class="border-2 border-orange-600 w-fit rounded-sm overflow-hidden"
+      class="border-2 border-orange-600 w-fit rounded-sm overflow-hidden bg-white"
       onsubmit={addToList}
     >
       <input
@@ -34,21 +36,31 @@
         placeholder="Entrez vos tâches"
         bind:value={newItem}
       />
-      <button type="submit" class="bg-orange-600 text-white py-2 px-3"
+      <button
+        type="submit"
+        class="bg-orange-600 text-white py-2 px-3 hover:bg-orange-700 transition-colors"
         >Add Task</button
       >
     </form>
-    <div class="mt-4">
+    <div class=" w-150">
       {#each taskList as task, index}
-        <div class="flex items-center gap-2">
+        <div
+          class="flex shadow-md mt-6 bg-white items-center gap-2 border-2 border-orange-600 p-8 hover:bg-gray-100 rounded-xl"
+        >
           <input
             type="checkbox"
             name="status"
             checked={task.status}
             onchange={() => swapStatus(index)}
-            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
           />
-          <p>{task.text}</p>
+          <p
+            class="flex-1 {task.status
+              ? 'line-through text-gray-500'
+              : 'text-gray-700'}"
+          >
+            {task.text}
+          </p>
           {#if task.status === false}
             <p class="text-red-500">à faire</p>
           {:else}
