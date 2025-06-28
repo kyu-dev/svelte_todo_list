a82e3b1cbc37138ff706315ef61e02b84cfc8d21
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-06-28 19:11:03 +0200

    Ajout d'un formulaire d'entrée dans le composant Input.svelte et intégration de ce composant dans la page principale pour permettre l'ajout de tâches dans l'application To-Do.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
-<div class="flex justify-center w-full">
+<script>
+  import Input from "../components/Input.svelte";
+</script>
+
+<div class="flex flex-col justify-center w-full">
   <h1 class="text-5xl font-semibold text-center py-20">
     To-Do APP <br />
     <span class="inline-flex gap-2 text-3xl font-normal"
       >Make with <img src="/logo.png" alt="logo" class="h-10" /></span
     >
   </h1>
+  <div class="w-full flex justify-center"><Input /></div>
 </div>
